091284900e46565ecbd8b8af669b76b11528f7ec
Babar Waheed <babar.waheed@gmail.com>
2019-11-18 14:03:30 +1100

    Addition of Mongoos, feedModel & Storing the post into MongoDB

--- routes/feed.js
+++ routes/feed.js
@@ -1,14 +1,14 @@
-var express = require('express');
+const express = require('express');
 const { body } = require('express-validator');
 
-var router = express.Router();
+const router = express.Router();
 const feedController = require('../controllers/feedController');
 
 /* GET /feed/posts */
 router.get('/posts', feedController.getPosts);
 
 /* POST /feed/posts */
-router.post('/post', [
+router.post('/post', [ 
     body('title')
     .trim()
     .isLength({min: 5}),
